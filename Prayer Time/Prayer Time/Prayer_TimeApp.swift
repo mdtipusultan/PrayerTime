b847b8c96103f8191cd8916a51ed6a34c94c61f5
Rezwan Chowdhury <tipu.diu.cse@gmail.com>
2024-09-04 17:34:50 +0600

    initial
    initial

---
+++ Prayer Time/Prayer Time/Prayer_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prayer_TimeApp.swift
+//  Prayer Time
+//
+//  Created by Rezwan Chowdhury on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Prayer_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
